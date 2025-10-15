c53a5517de2cbc137fc856e3b7513bd1a2030cb2
tiago <tiago_wolf@outlook.com>
2025-10-14 22:01:08 -0300

    fixing again

--- camera/corner_camera.gd
+++ camera/corner_camera.gd
@@ -8,4 +8,5 @@ func _on_body_entered(body: Node2D) -> void:
 		camera = body.get_node("Camera2D")
 		camera.limit_left = int(1.6*crop_radius)
 		camera.limit_top = int(-0.9*crop_radius)
+		camera.zoom = Vector2(0.6,0.6)
 		queue_free()
--- camera/crop_camera.gd
+++ camera/crop_camera.gd
@@ -9,4 +9,5 @@ func _on_body_entered(body: Node2D) -> void:
 		camera.limit_bottom -= crop_radius
 		camera.limit_top += int(-1.2*crop_radius)
 		camera.limit_left -= int(1.6*crop_radius)
+		camera.zoom = Vector2(1.0,1.0)
 		queue_free()
